b38cab92f12ac0c4a9bad6934dc5428a7601d790
Cristian Iván Aráoz <araozcristian@gmail.com>
2023-06-06 02:06:15 -0300

    Adding of Extra Fields for Tasa% logic

--- src/app/pagos-previsionales/pagos-previsionales.component.ts
+++ src/app/pagos-previsionales/pagos-previsionales.component.ts
@@ -8,16 +8,27 @@ import { Component } from '@angular/core';
 export class PagosPrevisionalesComponent {
   salarioBase: number = 0;
   tasaCotizacion: number = 0;
+  regimen: string = '';
+  categoriaLaboral: string = '';
+  empresa: string = '';
   pagoRealizado: boolean = false;
   montoPago: number = 0;
+  esRifRegimen: boolean = false;
 
   calcularMontoPago() {
+    if (this.regimen === 'RIF') {
+      this.tasaCotizacion = 5; // Tasa de cotización para el régimen RIF
+      this.esRifRegimen = true;
+    } else {
+      this.tasaCotizacion = 10; // Tasa de cotización predeterminada para otros regímenes
+      this.esRifRegimen = false;
+    }
+
     this.montoPago = this.salarioBase * (this.tasaCotizacion / 100);
   }
-
   realizarPago() {
     // Realizar cálculo del pago previsional
-    this.calcularMontoPago();
+    this.montoPago = this.salarioBase * (this.tasaCotizacion / 100);
 
     // Establecer la bandera de pago realizado como verdadera
     this.pagoRealizado = true;
